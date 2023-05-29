99de457c77c8590a5b0a203a92202a98b2c56836
KevinC911 <00157320@uca.edu.sv>
2023-05-29 09:43:47 -0600

    Fixing the playlist and user entities

--- Playlist/src/main/java/com/example/demo/models/entities/Playlist.java
+++ Playlist/src/main/java/com/example/demo/models/entities/Playlist.java
@@ -11,6 +11,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -35,17 +37,20 @@ public class Playlist {
 	@Column(name = "description")
 	private String description;
 	
-	@Column(name = "user_code")
-	private UUID userCode;
+	@ManyToOne(fetch = FetchType.EAGER)
+	@JoinColumn(name = "code_user", nullable = true)
+	private User user;
 	
 	@JsonIgnore
 	@OneToMany(mappedBy = "playlist", fetch = FetchType.LAZY)
 	List<SongXPlaylist> relationship;
-	
-	public Playlist(String title, String description, UUID userCode) {
+
+	public Playlist(String title, String description, User user) {
 		super();
 		this.title = title;
 		this.description = description;
-		this.userCode = userCode;
+		this.user = user;
 	}
+	
+	
 }
